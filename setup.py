f2f41430b0dcfde2132754ccebc03011f41b249f
engel <engeljavierpinto@gmail.com>
2020-06-21 12:37:59 -0400

    Creé el job para el despliegue en PyPiTest. También creé el archivo setup.py para construirn el paquete.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+import time
+import random
+from setuptools import setup
+
+
+def version():
+	if os.environ.get('GITHUB_ACTION'):
+		return os.environ.get('GITHUB_ACTION')
+	return random.randint(0, time.time())
+
+
+def url():
+    return "https:/github.com/{0}".format(os.environ.get("GITHUB_REPOSITORY"))
+
+
+setup(
+	name='transporte-uptbal',
+	author='Engel J. Pinto R.',
+	author_email='engeljavierpinto@gmail.com',
+	description='Una prueba con github actions',
+	version=version(),
+	packages=['transporte'],
+	url=url()
+)
